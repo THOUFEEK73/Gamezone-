1ec36d8e457fb3eae68eadbfc5d0b82200782139
Thoufeek <thoufeeknazeerpunalur@gmail.com>
2025-06-15 11:44:42 +0530

    Delete Some unneccecory folder and files

--- routes/gameDetailsRoutes.js
+++ routes/gameDetailsRoutes.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import {showAllGames,getDetailPage} from '../controllers/gameDetail.js'
-import isAthenticated from '../middleware/auth.js';
-const userRoutes = express.Router();
+// import express from 'express';
+// import {showAllGames,getDetailPage} from '../controllers/gameDetail.js'
+// import isAthenticated from '../middleware/auth.js';
+// const userRoutes = express.Router();
 
 
 
 
-userRoutes.get('/allgames',isAthenticated,showAllGames)
-userRoutes.get('/gamedetail/:id',isAthenticated,getDetailPage);
+// userRoutes.get('/allgames',isAthenticated,showAllGames)
+// userRoutes.get('/gamedetail/:id',isAthenticated,getDetailPage);
 
 
-export default userRoutes
+// export default userRoutes
