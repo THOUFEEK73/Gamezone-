088fed9fb4a2cd9bd9e54b40d55903850b10a450
Thoufeek <thoufeeknazeerpunalur@gmail.com>
2025-06-26 21:08:17 +0530

    version 0.4

--- config/passport.js
+++ config/passport.js
@@ -35,7 +35,7 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: process.env.GOOGLE_CALLBACK,
     proxy: true
 }, async (accessToken, refreshToken, profile, done) => {
     try {
